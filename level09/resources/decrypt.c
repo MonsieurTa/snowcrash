82945f66b4e32f9007c2deae53e408221d1ca998
William TA <willt29000@gmail.com>
2022-09-16 00:35:04 +0200

    add level 8 and 9

---
+++ level09/resources/decrypt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+        FILE *fp = NULL;
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t read = 0;
+
+        fp = fopen("./token", "r");
+        if (fp == NULL) {
+                exit(EXIT_FAILURE);
+        }
+        read = getline(&line, &len, fp);
+        printf("read: %d\n%s\n", read, line);
+        int i = 0;
+        while (line[i] && line[i] != '\n') {
+                line[i] -= i;
+                i++;
+        }
+        printf("%s\n", line);
+        return 0;
+}
